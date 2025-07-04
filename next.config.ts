d9b646aad88732ea73e73d846093454c111c2850
tungba103 <tungbt.personal@gmail.com>
2025-07-04 16:03:18 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import { crawlGoldPricesDaily } from "./scripts/crawl-gold-prices-daily";
-
-crawlGoldPricesDaily(); 
 
 const nextConfig: NextConfig = {
   /* config options here */
