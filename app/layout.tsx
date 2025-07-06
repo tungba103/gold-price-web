c68b3a09c1868088cb52cd8c3b9050bd521199db
tungba103 <tungbt.work@protonmail.com>
2025-07-06 22:58:26 +0700

    feat: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 // import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
@@ -9,8 +9,8 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
 // import {
 //   SidebarInset,
 //   SidebarProvider,
@@ -18,18 +18,21 @@ import { Separator } from "@/components/ui/separator"
 // } from "@/components/ui/sidebar"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Gold Price Vietnam",
-  description: "Gold Price Vietnam - Updated daily",
+  title: 'Gold Price Vietnam',
+  description: 'Gold Price Vietnam - Updated daily',
+  other: {
+    'apple-mobile-web-app-title': 'Gold Price Vietnam',
+  },
 };
 
 export default function RootLayout({
@@ -38,37 +41,38 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* <SidebarProvider> */}
-            {/* <AppSidebar variant="sidebar" /> */}
-            {/* <SidebarInset> */}
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                <div className="flex items-center gap-2 px-4">
-                    {/* <SidebarTrigger className="-ml-1" /> */}
-                    <Separator
-                    orientation="vertical"
-                    className="mr-2 data-[orientation=vertical]:h-4"
-                    />
-                    <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#" className="text-lg font-bold">
-                            Utilities
-                        </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
-                        <BreadcrumbPage className="text-lg font-bold">Gold Price</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                    </Breadcrumb>
-                </div>
-            </header>
-                {children}
-            {/* </SidebarInset> */}
+        {/* <AppSidebar variant="sidebar" /> */}
+        {/* <SidebarInset> */}
+        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
+          <div className='flex items-center gap-2 px-4'>
+            {/* <SidebarTrigger className="-ml-1" /> */}
+            <Separator
+              orientation='vertical'
+              className='mr-2 data-[orientation=vertical]:h-4'
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className='hidden md:block'>
+                  <BreadcrumbLink
+                    href='#'
+                    className='text-lg font-bold'
+                  >
+                    Utilities
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className='hidden md:block' />
+                <BreadcrumbItem>
+                  <BreadcrumbPage className='text-lg font-bold'>Gold Price</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        {children}
+        {/* </SidebarInset> */}
         {/* </SidebarProvider> */}
       </body>
     </html>
